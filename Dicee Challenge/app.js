0e43f361ab6dcdb6d0e67295b97f04fb1af4ad77
designexit <didmk.lab@gmail.com>
2023-07-20 14:51:06 +0900

    js 수업

--- Dicee Challenge/app.js
+++ Dicee Challenge/app.js
@@ -1,1 +1,21 @@
 // alert('1');
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+
+img1.setAttribute ('src', 'images/dice6.png');
+img2.setAttribute ('src', 'images/dice2.png');
+
+// let message ;
+// message = 'Hello';
+
+// alert('message');
+
+// const message = "Hello";
+// alert('message');
+
+const blue = '#555';
+const red = 'red';
+
+
+let color = blue;
+alert(color); 
